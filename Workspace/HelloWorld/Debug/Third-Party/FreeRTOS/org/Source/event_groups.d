ba6733464c4d259e05490fb082efb6b27e240454
OleksiiTimofieiev <contrbas@gmail.com>
2020-10-21 17:44:42 +0300

    RTOS

---
+++ Workspace/HelloWorld/Debug/Third-Party/FreeRTOS/org/Source/event_groups.d
@@ -0,0 +1,41 @@
+Third-Party/FreeRTOS/org/Source/event_groups.o: \
+ ../Third-Party/FreeRTOS/org/Source/event_groups.c \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Config/FreeRTOSConfig.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/timers.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/event_groups.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/timers.h
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Config/FreeRTOSConfig.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/timers.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/event_groups.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/timers.h:
---
+++ Workspace/HelloWorld/Debug/Third-Party/FreeRTOS/org/Source/stream_buffer.d
@@ -0,0 +1,32 @@
+Third-Party/FreeRTOS/org/Source/stream_buffer.o: \
+ ../Third-Party/FreeRTOS/org/Source/stream_buffer.c \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Config/FreeRTOSConfig.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h \
+ /Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/stream_buffer.h
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Config/FreeRTOSConfig.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+/Users/otimofie/Desktop/RTOS/Workspace/HelloWorld/Third-Party/FreeRTOS/org/Source/include/stream_buffer.h:
